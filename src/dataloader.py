fb1d2474c38fdb830dc0ab123bb179025e9e66d7
BertilleT <bertilletemple@yahoo.fr>
2024-05-02 15:32:07 +0200

    working on dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,9 +1,6 @@
-import torch
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from pathlib import Path
+from torch.utils.data import Dataset
 import rasterio
-from utils import *
+import numpy as np
 
 # create a dataloader. I need an argument to tell which level of classification I am working with, and load channel of mask accordingly
 class EcomedDataset(Dataset):
@@ -24,22 +21,6 @@ class EcomedDataset(Dataset):
             img = src.read()
         with rasterio.open(msk_path) as src:
             msk = src.read(self.level)
-        img = img.astype(np.int16)  # You can choose a different dtype if needed
+        img = img.astype(np.int16)
         return img, msk
-    
-# Create a dataloader for level 1
-img_dir = Path('/media/bertille/My Passport/natural_habitats_mapping/data/patch64/img/zone3_0_0')
-msk_dir = Path('/media/bertille/My Passport/natural_habitats_mapping/data/patch64/msk/l123/zone3_0_0')
-level = 1
-dataset = EcomedDataset(img_dir, msk_dir, level)
-dataloader = DataLoader(dataset, batch_size=11, shuffle=True)
-
-#plot one image and mask
-# Assuming dataloader is your PyTorch DataLoader instance
-for i, (img_batch, msk_batch) in enumerate(dataloader):
-    if i >= 10:
-        break  # Stop after processing 10 batches
-    
-    # list unique values in masks
-    print(np.unique(msk_batch.numpy()))
     
