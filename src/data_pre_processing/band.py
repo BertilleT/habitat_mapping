6d66da04c1f54674395c983f8513bc20fc3712f6
Bertille Temple <bertilletemple@yahoo.fr>
2024-06-10 09:27:42 +0200

    update from git repo hard drive

--- src/data_pre_processing/band.py
+++ src/data_pre_processing/band.py
@@ -1,7 +1,21 @@
-#load img_zone1_0_0_patch_2_12.tif and print metadata about channels rasterio
-import rasterio
+#plot one single image. 
 
-path = "../../data/patch256/img/zone1_0_0/img_zone1_0_0_patch_2_12.tif"
-with rasterio.open(path) as src:
-    # get metadata about channels
-    print(src.profile)
+import numpy as np
+import rasterio
+import matplotlib.pyplot as plt
+
+img_path = '../../data/full_img_msk/img/img_zone1_0_0.tif'
+with rasterio.open(img_path) as src:
+    img = src.read()
+    normalized_img = np.zeros_like(img, dtype=np.float32)
+    for c in range(4):
+        channel = img[c, :, :]
+        p2, p98 = np.percentile(channel, (2, 98))
+        channel = np.clip(channel, p2, p98)
+        channel = (channel - p2) / (p98 - p2)
+        normalized_img[c, :, :] = channel.astype(np.float32)
+    normalized_img = normalized_img[:3, :, :]
+    normalized_img = normalized_img.transpose(1, 2, 0)
+    plt.imshow(normalized_img)
+    #save
+    plt.savefig('one_full_img.png')
